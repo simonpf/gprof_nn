a3203d83fc2bc829787a2c107e14ae89ac79c377
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2021-05-17 11:41:25 +0200

    Added utils sub-module.

---
+++ gprof_nn/utils.py
@@ -0,0 +1,10 @@
+"""
+==============
+gprof_nn.utils
+==============
+
+Collection of utility attributes and functions.
+"""
+# Rectangular bounding box around continental united states (CONUS).
+CONUS = (-130, 20, -60.0, 55)
+
