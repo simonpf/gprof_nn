879a972349ec544ee56191a7a3b37f5372fedf8e
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2021-07-17 19:16:01 +0200

    Added tests for statistics.

--- gprof_nn/statistics.py
+++ gprof_nn/statistics.py
@@ -1,9 +1,10 @@
 """
 ===================
-gprof_nn.evaluation
+gprof_nn.statistics
 ===================
 
-This module provides functionality to evaluate retrieval results.
+This module provides a framework to calculate statistics over large
+datasets split across multiple files.
 """
 from abc import ABC, abstractmethod
 from concurrent.futures import ProcessPoolExecutor
@@ -12,14 +13,19 @@ from pathlib import Path
 import numpy as np
 import xarray as xr
 
+from gprof_nn.definitions import ALL_TARGETS
 from gprof_nn.data.retrieval import RetrievalFile
+
+###############################################################################
+# Statistics
+###############################################################################
 
 class Statistic(ABC):
     """
     Basic interface for statistics calculated across multiple retrieval files.
     """
     @abstractmethod
-    def process_data(self, data):
+    def process_file(self, sensor, filename):
         """
         Process data from a single file.
 
@@ -236,6 +242,7 @@ class GlobalMean(Statistic):
         output_file = destination / "global_mean.nc"
         data.to_netcdf(output_file)
 
+
 class PositionalZonalMean(Statistic):
     """
     Calculates zonal mean on a 1-degree latitude grid for all scan
@@ -305,16 +312,255 @@ class PositionalZonalMean(Statistic):
         data.to_netcdf(output_file)
 
 
-def process_files(files, statistics):
-    for f in files:
-        f = Path(f)
-        if ".BIN" in f.suffixes:
-            data = RetrievalFile(f).to_xarray_dataset()
-        else:
-            data = xr.open_dataset(f)
+LIMITS = {
+    "surface_precip": (1e-3, 2e2),
+    "convective_precip": (1e-3, 2e2),
+    "cloud_water_content": (1e-3, 2e2),
+    "rain_water_content": (1e-4, 2e1),
+    "snow_water_content": (1e-4, 2e1),
+    "latent_heat": (-200, 200),
+    "ice_water_path": (1e-3, 2e2),
+    "rain_water_path": (1e-3, 2e2),
+    "rain_water_path": (1e-3, 2e2),
+    "cloud_water_path": (1e-3, 2e2),
+}
 
+
+class TrainingDataStatistics(Statistic):
+    """
+    Class to calculate relevant statistics from training data files.
+    Calculates statistics of brightness temperatures, retrieval targets
+    as well as ancillary data.
+    """
+    def __init__(self):
+        pass
+
+    def _initialize_data(self,
+                         sensor,
+                         data):
+        n_freqs = sensor.n_freqs
+        self.has_angles = hasattr(sensor, "angles")
+        if self.has_angles:
+            n_angles = sensor.n_angles
+            self.angle_bins = np.zeros(sensor.angles.size + 1)
+            self.angle_bins[1:-1] = 0.5 * (sensor.angles[1:] +
+                                           sensor.angles[:-1])
+            self.angle_bins[0] = 2.0 * self.angle_bins[1] - self.angle_bins[2]
+            self.angle_bins[-1] = (2.0 * self.angle_bins[-2] -
+                                   self.angle_bins[-3])
+            self.tbs = np.zeros((18, n_freqs, n_angles, 300),
+                                dtype=np.float32)
+        else:
+            self.tbs = np.zeros((18, n_freqs, 300),
+                                dtype=np.float32)
+        self.tb_bins = np.linspace(100, 400, 301)
+
+        self.targets = {}
+        self.bins = {}
+        for k in ALL_TARGETS:
+            if k in data.variables:
+                # Surface and convective precip have angle depen
+                if (k in ["surface_precip", "convective_precip"] and
+                    self.has_angles):
+                    self.targets[k] = np.zeros((18, n_angles, 200),
+                                               dtype=np.float32)
+                else:
+                    self.targets[k] = np.zeros((18, 200),
+                                               dtype=np.float32)
+                l, h = LIMITS[k]
+                if l > 0:
+                    self.bins[k] = np.logspace(np.log10(l), np.log10(h), 201)
+                else:
+                    self.bins[k] = np.linspace(l, h, 201)
+
+        self.t2m = np.zeros((18, 200), dtype=np.float32)
+        self.t2m_bins = np.linspace(240, 330, 201)
+        self.tcwv = np.zeros((18, 200), dtype=np.float32)
+        self.tcwv_bins = np.linspace(0, 100, 201)
+        self.st = np.zeros(18, dtype=np.float32)
+        self.at = np.zeros(4, dtype=np.float32)
+
+
+    def process_file(self,
+                     sensor,
+                     filename):
+        """
+        Process data from a single file.
+
+        Args:
+            filename: The path of the data to process.
+        """
+        self.sensor = sensor
+        dataset = xr.load_dataset(filename)
+        if not hasattr(self, "tb_bins"):
+            self._initialize_data(sensor, dataset)
+
+        st = dataset["surface_type"]
+        for i in range(18):
+            i_st = (st == i + 1).data
+
+            # Sensor with varying EIA (cross track).
+            if self.has_angles:
+                tbs = (dataset["brightness_temperatures"] .data[i_st.data])
+                eia = dataset["earth_incidence_angle"].data[i_st, 0]
+
+                # For samples with real observations (snow + sea ice)
+                # observations must be selected based on earth incidence
+                # angle variable.
+                if (i + 1) in [2, 8, 9, 10, 11, 16]:
+                    for j in range(sensor.n_angles):
+                        l = self.angle_bins[j + 1]
+                        u = self.angle_bins[j]
+                        i_a = (eia >= l) * (eia < u)
+                        for k in range(sensor.n_freqs):
+                            cs, _ = np.histogram(tbs[i_a, k],
+                                                 bins=self.tb_bins)
+                            self.tbs[i, k, j] += cs
+                # For samples with simulated observations, values are already
+                # binned but bias correction must be applied.
+                else:
+                    tbs = (dataset["simulated_brightness_temperatures"]
+                           .data[i_st[:, :, 90:-90].data])
+                    b = (dataset["brightness_temperature_biases"]
+                         .data[i_st[:, :, 90:-90]])
+                    for j in range(sensor.n_angles):
+                        for k in range(sensor.n_freqs):
+                            x = tbs[:, j, k] - b[:, k]
+                            cs, _ = np.histogram(x, bins=self.tb_bins)
+                            self.tbs[i, k, j] += cs
+            # Sensor with constant EIA
+            else:
+                for j in range(sensor.n_freqs):
+                    cs, _ = np.histogram(tbs[:, j], bins=self.tb_bins)
+                    self.tbs[i, j] += cs
+
+            # Retrieval targets
+            for k in self.bins:
+                v = dataset[k].data
+                if v.shape[2] < i_st.shape[2]:
+                    inds = i_st[:, :, 90:-90]
+                else:
+                    inds = i_st
+                v = v[inds]
+                # Surface precip and convective precip must be treated
+                # separately.
+                if ((k in ["surface_precip", "convective_precip"]) and
+                    self.has_angles):
+                    if (i + 1) in [2, 8, 9, 10, 11, 16]:
+                        for j in range(sensor.n_angles):
+                            l = self.angle_bins[j + 1]
+                            u = self.angle_bins[j]
+                            i_a = (eia >= l) * (eia < u)
+                            cs, _ = np.histogram(v[i_a, 0],
+                                                 bins=self.bins[k])
+                            self.targets[k][i, j] += cs
+                    else:
+                        for j in range(sensor.n_angles):
+                            cs, _ = np.histogram(v[:, j], bins=self.bins[k])
+                            self.targets[k][i, j] += cs
+                else:
+                    cs, _ = np.histogram(v, bins=self.bins[k])
+                    self.targets[k][i] += cs
+
+            # Ancillary data
+            v = dataset["two_meter_temperature"].data[i_st]
+            cs, _ = np.histogram(v, bins=self.t2m_bins)
+            self.t2m[i] += cs
+            v = dataset["total_column_water_vapor"].data[i_st]
+            cs, _ = np.histogram(v, bins=self.tcwv_bins)
+            self.tcwv[i] += cs
+
+        bins = np.arange(0, 19) + 0.5
+        cs, _ = np.histogram(st, bins=bins)
+        self.st += cs
+        at = dataset["airmass_type"]
+        bins = np.arange(-1, 4) + 0.5
+        cs, _ = np.histogram(at, bins=bins)
+        self.at += cs
+
+    def merge(self, other):
+        """
+        Merge the data of this statistic with that calculated in a different
+        process.
+        """
+        if not hasattr(other, "tbs"):
+            return None
+        self.tbs += other.tbs
+        for k in self.targets:
+            self.targets[k] += other.targets[k]
+        self.t2m += other.t2m
+        self.tcwv += other.tcwv
+        self.st += other.st
+        self.at += other.at
+
+    def save(self, destination):
+        """
+        Save results to file in NetCDF format.
+        """
+        data = {}
+        tb_bins = 0.5 * (self.tb_bins[1:] + self.tb_bins[:-1])
+        data["brightness_temperature_bins"] = (
+            ("brightness_temperature_bins",),
+            tb_bins
+        )
+
+        if self.has_angles:
+            data["brightness_temperatures"] = (
+                ("surface_type_bins",
+                "channels",
+                "angles",
+                "brightness_temperature_bins"),
+                self.tbs
+            )
+        else:
+            data["brightness_temperatures"] = (
+                ("surface_type_bins",
+                "channels",
+                "brightness_temperature_bins"),
+                self.tbs
+            )
+
+        for k in self.targets:
+            bins = 0.5 * (self.bins[k][1:] + self.bins[k][:-1])
+            bin_dim = k + "_bins"
+            data[bin_dim] = (bin_dim,), bins
+            if (self.has_angles
+                and k in ["surface_precip", "convective_precip"]):
+                data[k] = ("surface_type", "angles", bin_dim), self.targets[k]
+            else:
+                data[k] = ("surface_type", bin_dim), self.targets[k]
+
+        bins = 0.5 * (self.t2m_bins[1:] + self.t2m_bins[:-1])
+        bin_dim = "two_meter_temperature_bins"
+        data[bin_dim] = (bin_dim,), bins
+        data["two_meter_temperature"] = ("surface_type", bin_dim), self.t2m
+
+        bins = 0.5 * (self.tcwv_bins[1:] + self.tcwv_bins[:-1])
+        bin_dim = "total_column_water_vapor_bins"
+        data[bin_dim] = (bin_dim,), bins
+        data["total_column_water_vapor"] = ("surface_type", bin_dim), self.tcwv
+
+        data["surface_type"] = ("surface_type_bins",), self.st
+        data["airmass_type"] = ("airmass_type_bins"), self.st
+
+        data = xr.Dataset(data)
+
+        destination = Path(destination)
+        output_file = (destination /
+                       (f"training_data_statistics_{self.sensor.name.lower()}"
+                        ".nc"))
+        data.to_netcdf(output_file)
+
+
+###############################################################################
+# Statistics
+###############################################################################
+
+
+def process_files(sensor, files, statistics):
+    for f in files:
         for s in statistics:
-            s.process_data(data)
+            s.process_file(sensor, f)
     return statistics
 
 
@@ -332,10 +578,21 @@ def _split_files(files, n):
         start = start + n_batch
 
 
+
 class StatisticsProcessor:
+    """
+    Class to manage the distributed calculation of statistics over a
+    range of files.
+
+    Attributes:
+        files: List of the files to process.
+        statistics: Statistics objects defining the statistics to compute.
+    """
     def __init__(self,
-            files,
-            statistics):
+                 sensor,
+                 files,
+                 statistics):
+       self.sensor = sensor
        self.files = files
        self.statistics = statistics
 
@@ -348,7 +605,10 @@ class StatisticsProcessor:
 
         tasks = []
         for b in batches:
-            tasks.append(pool.submit(process_files, b, self.statistics))
+            tasks.append(pool.submit(process_files,
+                                     self.sensor,
+                                     b,
+                                     self.statistics))
 
         stats = tasks.pop(0).result()
         for t in tasks:
@@ -357,18 +617,4 @@ class StatisticsProcessor:
 
         for s in stats:
             s.save(output_path)
-
-
-
-
-
-
-      
-
-
-
-
-
-
-
 
---
+++ test/test_statistics.py
@@ -0,0 +1,156 @@
+"""
+Tests for the gprof_nn.statistics module.
+"""
+from pathlib import Path
+
+import numpy as np
+import xarray as xr
+
+from gprof_nn import sensors
+from gprof_nn.statistics import (StatisticsProcessor,
+                                 TrainingDataStatistics)
+
+
+def test_training_statistics_gmi(tmpdir):
+    """
+    Ensure that TrainingDataStatistics class reproduces statistic of
+    test training data file.
+    """
+    data_path = Path(__file__).parent / "data"
+    files = [data_path / "training_data.nc"] * 2
+
+
+    stats = [TrainingDataStatistics()]
+    processor = StatisticsProcessor(sensors.GMI,
+                                    files,
+                                    stats)
+    processor.run(2, tmpdir)
+    input_data = xr.open_dataset(files[0])
+
+    results = xr.open_dataset(str(
+        tmpdir /
+        "training_data_statistics_gmi.nc"
+    ))
+
+    # Ensure TB dists match.
+    st = 1
+    bins = np.linspace(100, 400, 301)
+    i_st = (input_data.surface_type == 1).data
+    tbs = input_data["brightness_temperatures"].data[i_st]
+    counts_ref, _ = np.histogram(tbs[:, 0], bins=bins)
+    counts = results["brightness_temperatures"][st - 1, 0].data
+    assert np.all(np.isclose(counts, 2.0 * counts_ref))
+
+    # Ensure surface_precip dists match.
+    bins = np.logspace(-3, np.log10(2e2), 201)
+    i_st = (input_data.surface_type == 1).data
+    x = input_data["surface_precip"].data[i_st]
+    counts_ref, _ = np.histogram(x, bins=bins)
+    counts = results["surface_precip"][st - 1].data
+    assert np.all(np.isclose(counts, 2.0 * counts_ref))
+
+    # Ensure RWC distributions match.
+    bins = np.logspace(-4, np.log10(2e1), 201)
+    i_st = (input_data.surface_type == 1).data[:, :, 90:-90]
+    x = input_data["rain_water_content"].data[i_st]
+    counts_ref, _ = np.histogram(x, bins=bins)
+    counts = results["rain_water_content"][st - 1].data
+    assert np.all(np.isclose(counts, 2.0 * counts_ref))
+
+    # Ensure two-meter-temperature distributions match.
+    bins = np.linspace(240, 330, 201)
+    i_st = (input_data.surface_type == 1).data
+    x = input_data["two_meter_temperature"].data[i_st]
+    counts_ref, _ = np.histogram(x, bins=bins)
+    counts = results["two_meter_temperature"][st - 1].data
+    assert np.all(np.isclose(counts, 2.0 * counts_ref))
+
+    # Ensure surface type distributions match
+    bins = np.arange(19) + 0.5
+    x = input_data["surface_type"].data
+    counts_ref, _ = np.histogram(x, bins=bins)
+    counts = results["surface_type"].data
+    assert np.all(np.isclose(counts, 2.0 * counts_ref))
+
+
+def test_training_statistics_mhs(tmpdir):
+    """
+    Ensure that TrainingDataStatistics class reproduces statistic of
+    test training data file.
+    """
+    data_path = Path(__file__).parent / "data"
+    files = [data_path / "gprof_nn_mhs_era5_5.nc"] * 2
+
+
+    stats = [TrainingDataStatistics()]
+    processor = StatisticsProcessor(sensors.MHS,
+                                    files,
+                                    stats)
+    processor.run(2, tmpdir)
+    input_data = xr.open_dataset(files[0])
+
+    results = xr.open_dataset(str(
+        tmpdir /
+        "training_data_statistics_mhs.nc"
+    ))
+
+    # Ensure TB dists match.
+    st = 1
+    bins = np.linspace(100, 400, 301)
+
+    i_st_0 = ((input_data.source == 0) * (input_data.surface_type == 1)).data
+    i_st_0 = i_st_0[:, :, 90:-90]
+    tbs = input_data["simulated_brightness_temperatures"].data[i_st_0, 0, 0]
+    b = input_data["brightness_temperature_biases"].data[i_st_0, 0]
+    counts_ref, _ = np.histogram(tbs - b, bins=bins)
+    counts = results["brightness_temperatures"][st - 1, 0, 0].data
+    assert np.all(np.isclose(counts, 2.0 * counts_ref))
+
+    st = 2
+    sensor = sensors.MHS
+    angle_bins = np.zeros(sensor.angles.size + 1)
+    angle_bins[1:-1] = 0.5 * (sensor.angles[1:] + sensor.angles[:-1])
+    angle_bins[0] = 2.0 * angle_bins[1] - angle_bins[2]
+    angle_bins[-1] = 2.0 * angle_bins[-2] - angle_bins[-3]
+    l = angle_bins[1]
+    u = angle_bins[0]
+    i_st_1 = ((input_data.source != 0) *
+              (input_data.surface_type == 1) *
+              (input_data.earth_incidence_angle[..., 0] >= l) *
+              (input_data.earth_incidence_angle[..., 0] < u)).data
+    tbs = input_data["brightness_temperatures"].data[i_st_1, 0]
+    counts_ref, _ = np.histogram(tbs, bins=bins)
+    counts = results["brightness_temperatures"][st - 1, 0, 0].data
+    assert np.all(np.isclose(counts, 2.0 * counts_ref))
+
+    # Ensure surface_precip dists match.
+    st = 1
+    bins = np.logspace(-3, np.log10(2e2), 201)
+    i_st = (input_data.surface_type == 1).data
+    x = input_data["surface_precip"].data[i_st, 0]
+    counts_ref, _ = np.histogram(x, bins=bins)
+    counts = results["surface_precip"][st - 1, 0].data
+    assert np.all(np.isclose(counts, 2.0 * counts_ref))
+
+    ## Ensure RWC distributions match.
+    #bins = np.logspace(-4, np.log10(2e1), 201)
+    #i_st = (input_data.surface_type == 1).data[:, :, 90:-90]
+    #x = input_data["rain_water_content"].data[i_st]
+    #counts_ref, _ = np.histogram(x, bins=bins)
+    #counts = results["rain_water_content"][st - 1].data
+    #assert np.all(np.isclose(counts, 2.0 * counts_ref))
+
+    ## Ensure two-meter-temperature distributions match.
+    #bins = np.linspace(240, 330, 201)
+    #i_st = (input_data.surface_type == 1).data
+    #x = input_data["two_meter_temperature"].data[i_st]
+    #counts_ref, _ = np.histogram(x, bins=bins)
+    #counts = results["two_meter_temperature"][st - 1].data
+    #assert np.all(np.isclose(counts, 2.0 * counts_ref))
+
+    ## Ensure surface type distributions match
+    #bins = np.arange(19) + 0.5
+    #x = input_data["surface_type"].data
+    #counts_ref, _ = np.histogram(x, bins=bins)
+    #counts = results["surface_type"].data
+    #assert np.all(np.isclose(counts, 2.0 * counts_ref))
