88bdf6af389ef37585c05ddbbe605c935f39bbee
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2021-07-19 14:12:01 +0200

    Added script to calculate observation statistics.

--- scripts/calculate_observation_statistics.py
+++ scripts/calculate_observation_statistics.py
@@ -1,5 +1,6 @@
 """
-Script to calculate statistics for bin files.
+Script to extract brightness temperature statistics from observation
+datasets in NetCDF format.
 """
 import argparse
 from pathlib import Path
@@ -13,19 +14,20 @@ from gprof_nn import statistics
 
 parser = argparse.ArgumentParser(
     description=(
-        "Calculate statistics for bin files for a given sensor."
+        "Calculate statistics for all observation dataset files in the"
+        "folder."
     )
 )
+
 parser.add_argument('sensor',
                     metavar="sensor",
                     type=str,
                     help='Name of the sensor.')
-parser.add_argument('input_paths',
-                    metavar="path_1, path_2, ...",
+parser.add_argument('input_path',
+                    metavar="path",
                     type=str,
-                    nargs="*",
-                    help='Directories containing the bin files.'
-                    'to process')
+                    help=("Root of the folder tree containing the observation"
+                          "dataset files."))
 parser.add_argument('output_path',
                     metavar="output_path",
                     type=str,
@@ -37,17 +39,14 @@ parser.add_argument('--n_processes',
                     help='The number of processes to use for the processing.')
 
 args = parser.parse_args()
-input_paths = args.input_paths
+input_paths = Path(args.input_paths)
 output_path = Path(args.output_path)
-n_procs = parser.n_processes
-
 sensor = getattr(sensors, args.sensor, None)
+n_procs = parser.n_processes
 if sensor is None:
     raise ValueError(f"Sensor {args.sensor} is currently not supported.")
 
-stats = [statistics.BinFileStatistics()]
-input_files = []
-for path in input_paths:
-    input_files += list(Path(path).glob("**/*.bin"))
+stats = [statistics.ObservationStatistics()]
+input_files = list(Path(input_path).glob("**/*.nc"))
 processor = statistics.StatisticsProcessor(sensor, input_files, stats)
 processor.run(n_procs, output_path)
