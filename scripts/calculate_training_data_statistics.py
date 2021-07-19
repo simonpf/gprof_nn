d765cde87fb8d68a4632cb06cb9f82e3ef71eb1c
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2021-07-19 04:25:40 -0600

    Some fixes for statistics extraction script.

--- scripts/calculate_training_data_statistics.py
+++ scripts/calculate_training_data_statistics.py
@@ -1,7 +1,8 @@
 import argparse
 from pathlib import Path
 
-from gprof_nn import evaluation
+from gprof_nn import sensors
+from gprof_nn import statistics
 #
 # Parse arguments
 #
@@ -12,6 +13,8 @@ parser = argparse.ArgumentParser(
         "NetCDF files."
         )
 )
+parser.add_argument('sensor', metavar="sensor", type=str,
+                    help='Name of the sensor.')
 parser.add_argument('input_path', metavar="input_path", type=str,
                     help='Root of the directory tree containing the files'
                     'to process')
@@ -21,19 +24,14 @@ parser.add_argument('output_path', metavar="output_path", type=str,
 args = parser.parse_args()
 input_path = Path(args.input_path)
 output_path = Path(args.output_path)
-stats = args.statistics
-pattern = args.pattern
 
-statistics = [TrainingDataStatistics()]
-for s in stats:
-    statistic = getattr(evaluation, s, None)
-    if statistic is None:
-        raise ValueError(f"Coud not find statistic '{s}'.")
-    statistics.append(statistic())
+sensor = getattr(sensors, args.sensor, None)
+if sensor is None:
+    raise ValueError(f"Sensor {args.sensor} is currently not supported.")
 
-
+stats = [statistics.TrainingDataStatistics()]
 input_files = list(Path(input_path).glob("**/*.nc"))
 print(input_files)
-processor = evaluation.StatisticsProcessor(input_files, statistics)
+processor = statistics.StatisticsProcessor(sensor, input_files, stats)
 processor.run(4, output_path)
 
