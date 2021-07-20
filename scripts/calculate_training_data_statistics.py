7cb233435c82d3fcb54db9c3b1d790ac3ebd17c1
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2021-07-20 07:16:46 -0600

    Fix extraction of training data statistics.

--- scripts/calculate_training_data_statistics.py
+++ scripts/calculate_training_data_statistics.py
@@ -11,21 +11,8 @@ from gprof_nn import statistics
 
 parser = argparse.ArgumentParser(
     description=(
-        "Run legacy GPROF on GPROF-NN 0D input data and store results as"
-        "NetCDF files."
+        "Extract statistics form training data files."
     )
-)
-parser.add_argument(
-    "input_path",
-    metavar="input_path",
-    type=str,
-    help="Root of the directory tree containing the files" "to process",
-)
-parser.add_argument(
-    "output_path",
-    metavar="output_path",
-    type=str,
-    help="The folder to which to write the results.",
 )
 parser.add_argument('sensor', metavar="sensor", type=str,
                     help='Name of the sensor.')
@@ -34,10 +21,16 @@ parser.add_argument('input_path', metavar="input_path", type=str,
                     'to process')
 parser.add_argument('output_path', metavar="output_path", type=str,
                     help='The folder to which to write the results.')
+parser.add_argument('--n_processes',
+                    metavar="n",
+                    type=int,
+                    default=4,
+                    help='The number of processes to use for the processing.')
 
 args = parser.parse_args()
 input_path = Path(args.input_path)
 output_path = Path(args.output_path)
+n_procs = args.n_processes
 
 #
 # Find files and process
@@ -49,7 +42,9 @@ sensor = getattr(sensors, args.sensor, None)
 if sensor is None:
     raise ValueError(f"Sensor {args.sensor} is currently not supported.")
 
-stats = [statistics.TrainingDataStatistics()]
+stats = [statistics.TrainingDataStatistics(),
+         statistics.ZonalDistribution(),
+         statistics.GlobalDistribution()]
 input_files = list(Path(input_path).glob("**/*.nc"))
 processor = statistics.StatisticsProcessor(sensor, input_files, stats)
-processor.run(4, output_path)
+processor.run(n_procs, output_path)
