70c02af1870a28b9b909c2ddf4e69179d33de0b4
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2021-07-23 13:56:13 +0200

    Added test for new augmentation functionality.

--- test/test_augmentation.py
+++ test/test_augmentation.py
@@ -2,11 +2,27 @@
 Tests for the data augmentation methods in gprof_nn.augmentation.
 """
 import numpy as np
-from gprof_nn.augmentation import (get_center_pixels,
+from gprof_nn.augmentation import (GMI_GEOMETRY,
+                                   get_center_pixels,
                                    get_transformation_coordinates,
                                    offset_x,
                                    M,
                                    N)
+
+
+def test_gmi_geometry():
+    """
+    Assert that coordinate transformation function for GMI viewing
+    geometry are reversible.
+    """
+    i = np.arange(-110, 111, 10)
+    j = np.arange(-110, 111, 10)
+    ij = np.stack(np.meshgrid(i, j))
+    xy = GMI_GEOMETRY.pixel_coordinates_to_euclidean(ij)
+    ij_r = GMI_GEOMETRY.euclidean_to_pixel_coordinates(xy)
+    assert np.all(np.isclose(ij, ij_r))
+
+
 
 def test_center_pixels():
     c_o, c_i = get_center_pixels(-1.0, -1.0)
