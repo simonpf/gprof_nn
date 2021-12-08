2793078f96567dcbcfc288aa47f04133ee7b8c35
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2021-12-08 15:45:58 -0700

    Added app_dir package to dependencies.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     url="https://github.com/simonpf/gprof_nn",
     author="Simon Pfreundschuh",
     author_email="simon.pfreundschuh@chalmers.se",
-    install_requires=["numpy", "xarray", "torch"],
+    install_requires=["numpy", "xarray", "torch", "appdirs"],
     entry_points = {
         'console_scripts': ['gprof_nn=gprof_nn.bin:gprof_nn'],
     },
