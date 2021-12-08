563b9be6ac52c26f5ff99cb1a113f3fd81f36821
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2021-12-08 15:43:29 -0700

    Added functionality to fetch models from a server.

---
+++ gprof_nn/data/__init__.py
@@ -0,0 +1,82 @@
+"""
+=============
+gprof_nn.data
+=============
+
+The data module provides functionality to dynamically download
+data needed by the module.
+
+Its sub-modules provide functionality to read and process various
+data formats required for the processing of training data.
+"""
+from pathlib import Path
+import shutil
+import urllib
+
+from appdirs import user_config_dir
+
+_DATA_DIR = Path(user_config_dir("pansat", "pansat"))
+_DATA_DIR.mkdir(parents=True, exist_ok=True)
+
+_MODEL_DIR = _DATA_DIR / "models"
+_MODEL_DIR.mkdir(parents=True, exist_ok=True)
+
+_DATA_URL = "http://0.0.0.0:8000/"
+
+
+def get_file(path):
+    """
+    Get file from user's data directory or download it if necessary.
+
+    Args:
+
+        path: Path of the file relative to the root of the data
+            directory.
+
+    Return:
+        A absolute local path pointing to the file.
+    """
+    local_path = _DATA_DIR / path
+    if not local_path.exists():
+        url = _DATA_URL + str(path)
+        with urllib.request.urlopen(url) as response:
+            with open(local_path, "wb") as output:
+                shutil.copyfileobj(response, output)
+
+    return local_path
+
+
+def get_model_path(kind, sensor, configuration):
+    """
+    Get path to local GPROF-NN model.
+
+    Args:
+        kind: The type of the GPROF-NN model ('1D' or '3D')
+        sensor: Sensor object representing the sensor for which
+            to retrieve the model.
+        configuration: The configuration of the model
+            ('ERA5' or 'GANAL').
+
+    Return:
+        Local path to the requested model.
+    """
+    kind = kind.lower()
+    if kind not in ["1d", "2d"]:
+        raise ValueError("'kind' must be one of: '1D', '3D'")
+    configuration = configuration.lower()
+    if configuration not in ["era5", "ganal"]:
+        raise ValueError("'configuration' must be one of: 'ERA5', 'GANAL'")
+
+    sensor_name = sensor.full_name.lower()
+    model_name = f"gprof_nn_{kind}_{sensor_name}_{configuration}.pckl"
+    path = Path("models") / model_name
+
+    try:
+        return get_file(path)
+    except urllib.error.HTTPError:
+        pass
+
+    sensor_name = sensor.sensor_name.lower()
+    model_name = f"gprof_nn_{kind}_{sensor_name}_{configuration}.pckl"
+    path = Path("models") / model_name
+    return get_file(path)
