059402273ade8ff36fb6ac442c4aa2fd9ff59264
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2021-12-22 12:43:56 -0700

    Silence wget output.

--- test/test_retrieval_gmi.sh
+++ test/test_retrieval_gmi.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-wget http://rain.atmos.colostate.edu/gprof_nn/test/gmi_era5_harvey.pp
+wget -q http://rain.atmos.colostate.edu/gprof_nn/test/gmi_era5_harvey.pp
 echo "Runing GPROF-NN 1D retrieval."
 gprof_nn 1d ERA5 gmi_era5_harvey.pp
 echo "Runing GPROF-NN 3D retrieval."
