78edd80efc7996751bc7fed3be47c51399455342
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2021-12-27 17:51:18 -0700

    Add tests for TMI sensor.

--- test/test_sensors.py
+++ test/test_sensors.py
@@ -12,6 +12,7 @@ from gprof_nn.data.training_data import decompress_and_load
 
 TEST_FILE_GMI = Path("gmi") / "gprof_nn_gmi_era5.nc.gz"
 TEST_FILE_MHS = Path("mhs") / "gprof_nn_mhs_era5.nc.gz"
+TEST_FILE_TMI = Path("tmi") / "gprof_nn_tmi_era5.nc.gz"
 
 
 DATA_PATH = get_test_data_path()
@@ -57,7 +58,7 @@ def test_smooth_gmi_field():
     assert np.all(np.isclose(field_s[:, :, 0], field_s[:, :, 1], atol=1e-3))
 
 
-def test_load_training_data_gmi():
+def test_load_training_data_1d_gmi():
     """
     Ensure that loading the training data for GMI produces realistic
     values.
@@ -102,7 +103,52 @@ def test_load_training_data_gmi():
     assert np.all(np.isclose(at.sum(axis=1), 1.0))
 
 
-def test_load_training_data_mhs():
+def test_load_training_data_3d_gmi():
+    """
+    Ensure that loading the training data for GMI produces realistic
+    values.
+    """
+    input_file = DATA_PATH / TEST_FILE_GMI
+    input_data = decompress_and_load(input_file)
+
+    sensor = sensors.GMI
+
+    targets = ["surface_precip", "rain_water_content"]
+    rng = np.random.default_rng()
+
+    x, y = sensor.load_training_data_3d(input_data, targets, False, rng)
+
+    # TB ranges
+    assert np.all(x[:, :5] > 20)
+    assert np.all(x[:, :5] < 500)
+    # Two-meter temperature
+    assert np.all(x[:, 15] > 200)
+    assert np.all(x[:, 15] < 400)
+    # TCWV
+    assert np.all(x[:, 16] >= 0)
+    assert np.all(x[:, 16] < 100)
+
+    # Assert all targets are loaded
+    assert all(t in y for t in targets)
+
+    # Ensure that loaded surface precip is within the range given
+    # of the surface precip observed for the different angles.
+    sp = y["surface_precip"]
+    mask = sp >= 0
+    assert np.all(sp[mask] < 500)
+
+    tbs = x[:, :15]
+    valid = tbs >= 0
+    assert np.all((tbs[valid] > 20) * (tbs[valid] < 400))
+
+    st = x[:, -22:-4] > 0
+    assert np.all(np.isclose(st.sum(axis=1), 1.0))
+
+    at = x[:, -4:] > 0
+    assert np.all(np.isclose(at.sum(axis=1), 1.0))
+
+
+def test_load_training_data_1d_mhs():
     """
     Ensure that loading the training data for MHS produces realistic
     values.
@@ -134,6 +180,44 @@ def test_load_training_data_mhs():
     assert all(t in y for t in targets)
 
 
+def test_load_training_data_3d_mhs():
+    """
+    Ensure that loading the training data for MHS produces realistic
+    values.
+    """
+    input_file = DATA_PATH / TEST_FILE_MHS
+    input_data = decompress_and_load(input_file)
+
+    sensor = sensors.MHS
+
+    targets = ["surface_precip", "rain_water_content"]
+    rng = np.random.default_rng()
+
+    x, y = sensor.load_training_data_3d(input_data, targets, False, rng)
+
+    # Two-meter temperature
+    valid = np.isfinite(x[:, 6])
+    assert np.all(x[:, 6][valid] > 150)
+    assert np.all(x[:, 6][valid] < 400)
+    # TCWV
+    valid = np.isfinite(x[:, 7])
+    assert np.all(x[:, 7][valid] >= 0)
+    assert np.all(x[:, 7][valid] < 100)
+
+    # Assert all targets are loaded
+    assert all(t in y for t in targets)
+
+    # Ensure that loaded surface precip is within the range given
+    # of the surface precip observed for the different angles.
+    sp = y["surface_precip"]
+    mask = sp >= 0
+    assert np.all(sp[mask] < 500)
+
+    tbs = x[:, :5]
+    valid = tbs >= 0
+    assert np.all((tbs[valid] > 20) * (tbs[valid] < 400))
+
+
 def test_interpolation_mhs(tmp_path):
     """
     Ensure that interpolation of surface precipitation
@@ -163,3 +247,73 @@ def test_interpolation_mhs(tmp_path):
 
     inds = (sp > 1.0) * (sp < 8.0)
     assert np.all(np.isclose(va[inds], sp[inds]))
+
+
+def test_load_training_data_1d_tmi():
+    """
+    Ensure that loading the training data for TMI produces realistic
+    values.
+    """
+    DATA_PATH = Path(__file__).parent / "data"
+    input_file = DATA_PATH / TEST_FILE_TMI
+    input_data = decompress_and_load(input_file)
+
+    sensor = sensors.TMI
+
+    targets = ["surface_precip", "rain_water_content"]
+    rng = np.random.default_rng()
+
+    x, y = sensor.load_training_data_1d(input_data, targets, False, rng)
+
+    # TB ranges
+    assert np.all(x[:, :9] > 20)
+    assert np.all(x[:, :9] < 500)
+    # Two-meter temperature
+    assert np.all(x[:, 9] > 200)
+    assert np.all(x[:, 9] < 400)
+    # TCWV
+    assert np.all(x[:, 10] >= 0)
+    assert np.all(x[:, 10] < 100)
+
+    # Assert all targets are loaded
+    assert all(t in y for t in targets)
+
+
+def test_load_training_data_3d_tmi():
+    """
+    Ensure that loading the training data for TMI produces realistic
+    values.
+    """
+    DATA_PATH = Path(__file__).parent / "data"
+    input_file = DATA_PATH / TEST_FILE_TMI
+    input_data = decompress_and_load(input_file)
+
+    sensor = sensors.TMI
+
+    targets = ["surface_precip", "rain_water_content"]
+    rng = np.random.default_rng()
+
+    x, y = sensor.load_training_data_3d(input_data, targets, False, rng)
+
+    # Two-meter temperature
+    valid = np.isfinite(x[:, 6])
+    assert np.all(x[:, 9][valid] > 150)
+    assert np.all(x[:, 9][valid] < 400)
+    # TCWV
+    valid = np.isfinite(x[:, 7])
+    assert np.all(x[:, 10][valid] >= 0)
+    assert np.all(x[:, 10][valid] < 100)
+
+    # Assert all targets are loaded
+    assert all(t in y for t in targets)
+
+    # Ensure that loaded surface precip is within the range given
+    # of the surface precip observed for the different angles.
+    sp = y["surface_precip"]
+    mask = sp >= 0
+    assert np.all(sp[mask] < 500)
+
+    tbs = x[:, :5]
+    valid = tbs >= 0
+    assert np.all((tbs[valid] > 20) * (tbs[valid] < 400))
+
