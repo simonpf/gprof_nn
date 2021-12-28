8812fcf98d5162958f28ca465b3ec84a3db246ca
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2021-12-27 17:50:58 -0700

    Run only 1D retrieval in CI.

--- test/test_retrieval_gmi.sh
+++ test/test_retrieval_gmi.sh
@@ -2,5 +2,5 @@
 wget -q http://rain.atmos.colostate.edu/gprof_nn/test/gmi_era5_harvey.pp
 echo "Runing GPROF-NN 1D retrieval."
 gprof_nn 1d ERA5 gmi_era5_harvey.pp
-echo "Runing GPROF-NN 3D retrieval."
-gprof_nn 3d ERA5 gmi_era5_harvey.pp
+#echo "Runing GPROF-NN 3D retrieval."
+#gprof_nn 3d ERA5 gmi_era5_harvey.pp
