78da5c5118efc0c9541dd134e0ae7e9c34b57b4d
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2022-01-11 11:49:05 -0700

    Make sure package data is included in install.

--- setup.py
+++ setup.py
@@ -16,12 +16,20 @@ setup(
     author_email="simon.pfreundschuh@chalmers.se",
     install_requires=[
         "numpy", "scipy", "xarray", "torch", "appdirs", "rich",
-        "quantnn>=0.0.2dev"
+        "quantnn>=0.0.4dev"
     ],
     entry_points = {
         'console_scripts': ['gprof_nn=gprof_nn.bin:gprof_nn'],
     },
     packages=find_packages(),
+    package_data={
+        "gprof_nn": [
+            "files/gmi_era5_sensitivities.npy",
+            "files/mhs_era5_sensitivities.npy",
+            "files/preprocessor_template.pp",
+            "files/matplotlib_style.rc",
+        ]
+    },
     python_requires=">=3.6",
     project_urls={
         "Source": "https://github.com/simonpf/gprof_nn/",
