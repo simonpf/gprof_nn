9b9f8b5a8055fd6df4a07336234cf5345ae87ccb
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2022-03-23 13:41:03 -0600

    Fix MRMS record for SSMIS.

--- gprof_nn/data/types.py
+++ gprof_nn/data/types.py
@@ -9,6 +9,7 @@ used for the GPROF processing at CSU.
 import numpy as np
 
 CONICAL = "CONICAL"
+CONICAL_CONST = "CONICAL_CONSTELLATION"
 XTRACK = "XTRACK"
 
 
@@ -353,8 +354,39 @@ def get_mrms_file_record(n_chans, n_angles, kind):
             ("n_rain", "i4"),
             ("n_snow", "i4"),
             ("fraction_missing", "f4"),
-            ("brightness_temperatures", f"{n_chans}f4"),
+            ("brightness_temperatures", f"15f4"),
         ])
+    elif kind == CONICAL_CONST:
+        dtype = np.dtype([
+                ("datasetnum", "i4"),
+                ("latitude", "f4"),
+                ("longitude", "f4"),
+                ("orbitnum", "i4"),
+                ("n_pixels", "i4"),
+                ("n_scans", "i4"),
+                ("scan_time", f"5i4"),
+                ("skin_temperature", f"i4"),
+                ("total_column_water_vapor", f"i4"),
+                ("surface_type", f"i4"),
+                ("quality_flag", f"f4"),
+                ("two_meter_temperature", "f4"),
+                ("wet_bulb_temperature", "f4"),
+                ("lapse_rate", "f4"),
+                ("surface_precip", "f4"),
+                ("surface_rain", "f4"),
+                ("convective_rain", "f4"),
+                ("stratiform_rain", "f4"),
+                ("snow", "f4"),
+                ("quality_index", "f4"),
+                ("gauge_fraction", "f4"),
+                ("standard_deviation", "f4"),
+                ("n_stratiform", "i4"),
+                ("n_convective", "i4"),
+                ("n_rain", "i4"),
+                ("n_snow", "i4"),
+                ("fraction_missing", "f4"),
+                ("brightness_temperatures", f"15f4"),
+            ])
     else:
         dtype = np.dtype([
                 ("datasetnum", "i4"),
