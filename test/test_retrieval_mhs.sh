6529c49cb0473d2a44994cf468611c733a125d5f
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2023-03-28 09:14:38 +0200

    Updated URLS.

--- test/test_retrieval_mhs.sh
+++ test/test_retrieval_mhs.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-wget http://rain.atmos.colostate.edu/gprof_nn/test/mhs_era5_harvey.pp
+wget https://rain.atmos.colostate.edu/gprof_nn/test/mhs_era5_harvey.pp
 echo "Running GPROF-NN 1D retrieval."
 gprof_nn 1d ERA5 mhs_era5_harvey.pp
 echo "Running GPROF-NN 3D retrieval."
