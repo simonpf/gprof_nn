7f59e7e0c6c3f14b74543d58e09e54498fb16612
Simon Pfreundschuh <s.pfreundschuh@gmail.com>
2023-06-22 15:01:21 -0600

    Add option to train NN on simulated TBs.

--- gprof_nn/retrieval.py
+++ gprof_nn/retrieval.py
@@ -1486,7 +1486,6 @@ class BinFileLoader:
         self.sensor = self.bin_file.sensor
 
         data = BinFile(filename).to_xarray_dataset()
-        data["brightness_temperatures"] += data["delta_tb"]
         self.data = data
 
         self.n_samples = self.data.samples.size
