Fix weighting of CMB training data.

gprof_nn/data/training_data.py
@@ -2363,16 +2363,16 @@ class GPROFNNHRDataset:
             )
             if self.global_weights is not None:
                 weights = self.global_weights.interp(latitude=scene.latitude, longitude=scene.longitude).data
-                weights *= self.scene_weights_cmb
+                weights *= self.scene_weights_cmb / self.scene_weights_cloudsat
                 weights = torch.tensor(weights.astype(np.float32))[None, None]
             else:
                 weights = torch.ones((96, 96))[None, None]
         else:
             valid_ref = surface_precip <= self.precip_threshold
-            if self.global_weights is not None:
-                weights = torch.tensor(self.scene_weights_cloudsat.astype(np.float32))[None, None]
-            else:
-                weights = torch.ones((96, 96))[None, None]
+            weights = torch.ones((96, 96))[None, None]
+            #if self.global_weights is not None:
+            #    weights = torch.tensor(self.scene_weights_cloudsat.astype(np.float32))[None, None]
+            #else:
 
 
         surface_precip[~valid_ref] = np.nan
@@ -2387,10 +2387,13 @@ class GPROFNNHRDataset:
         chans_in = np.random.permutation(scene.input_observations.all_channels.size)
 
         if self.rng.random() > 0.5:
-            width = self.rng.integers(0, 25)
+            width = self.rng.integers(1, 25)
         else:
             width = 0
         left = self.rng.random()
+
+        missing_obs = np.any(np.isnan(input_observations), 0)
+        surface_precip[missing_obs] = np.nan
 
         for input_ind in range(self.seq_len_in):
             rand = self.rng.random()