3125ff696735be9825a98a4ae04818ba7501c938
Simon Pfreundschuh <s.pfreundschuh@gmail.com>
2025-02-09 20:44:55 -0700

    Update version of MHS preprocessor.

--- gprof_nn/data/preprocessor.py
+++ gprof_nn/data/preprocessor.py
@@ -718,7 +718,7 @@ def has_preprocessor():
 # Dictionary mapping sensor IDs to preprocessor executables.
 PREPROCESSOR_EXECUTABLES = {
     "GMI": "gprof2024pp_GMI_L1C",
-    "MHS": "gprof2023pp_MHS_L1C",
+    "MHS": "gprof2024pp_MHS_L1C",
     "TMIPR": "gprof2021pp_TMI_L1C",
     "TMIPO": "gprof2021pp_TMI_L1C",
     "SSMI": "gprof2021pp_SSMI_L1C",
