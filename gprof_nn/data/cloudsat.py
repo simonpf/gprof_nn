52b0f263f38aa16082de71f686205c478cb097a6
Simon Pfreundschuh <s.pfreundschuh@gmail.com>
2025-02-22 19:52:32 -0700

    Fix ERA5 interpolation.

--- gprof_nn/data/cloudsat.py
+++ gprof_nn/data/cloudsat.py
@@ -201,8 +201,8 @@ def extract_cloudsat_scenes(
 
         rain = (1 <= pflag) * (pflag <= 3)
         era5_data = load_era5_data(input_data.scan_time.min().data, input_data.scan_time.max().data)
-        era5_data = era5_data.interp(latitude=input_data.latitude, longitude=input_data.longitude)
-        total_precip[rain] = era5_data.tp.data[rain]
+        era5_precip = era5_data.tp.interp(latitude=input_data.latitude, longitude=input_data.longitude).data
+        total_precip[rain] = 1000.0 * era5_precip[rain]
 
         input_data["total_precip"] = (("scans", "pixels"), total_precip)
 
