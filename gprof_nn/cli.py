12066e40a3204c779e8771d35c42d02814b200b9
Simon Pfreundschuh <s.pfreundschuh@gmail.com>
2025-04-07 10:18:19 -0600

    Add functions to download test data.

--- gprof_nn/cli.py
+++ gprof_nn/cli.py
@@ -5,15 +5,19 @@ gprof_nn.cli
 This module implements the command line interfaces for the functionality
 provided by the 'gprof_nn' package.
 """
+
 import click
 import gprof_nn.logging
 import gprof_nn.config as conf
 from gprof_nn import training
 from gprof_nn import retrieval, testing
+from gprof_nn import download as dl
+
 
 @click.group()
 def gprof_nn():
     pass
+
 
 # Make data extraction commands available only if pansat is installed.
 try:
@@ -24,7 +28,7 @@ try:
         era5,
         finetuning,
         cloudsat,
-        combined
+        combined,
     )
 
     @gprof_nn.group(name="extract_training_data")
@@ -46,9 +50,49 @@ except ImportError:
 # gprof_nn config
 ######################################################################
 
+
 @gprof_nn.group(help="Inspect and change the local GPROF-NN configuration.")
 def config():
     pass
+
+
+config.command(name="file", help="Show location of configuration file.")(conf.file)
+config.command(name="show", help="Show current configuration.")(conf.show_config)
+config.command(name="set", help="Modify the configuration.")(conf.set_config)
+
+######################################################################
+# gprof_nn download
+######################################################################
+
+
+@gprof_nn.group(help="Inspect and change the local GPROF-NN configuration.")
+def download():
+    pass
+
+
+@download.command(name="model")
+@click.argument("sensor", type=str)
+def download_model(sensor: str):
+    """
+    Download GPROF-NN retrieval model for SENSOR. Use SENSOR=all to download the models for all sensors.
+    """
+    if sensor.lower() == "all":
+        sensor_list = sensors.all_sensors()
+    else:
+        sensor_list = sensor.get_sensor(sensor)
+    for sensor in sensor_list:
+        dl.download_model(sensor.name)
+
+
+@download.command(name="test_data")
+@click.argument("sensor", type=str)
+@click.argument("kind", type=str)
+def download_test_data(sensor: str, kind: str):
+    """
+    Download GPROF-NN retrieval test data of type KIND for SENSOR.
+    """
+    dl.download_test_file(sensor, kind)
+
 
 config.command(name="file", help="Show location of configuration file.")(conf.file)
 config.command(name="show", help="Show current configuration.")(conf.show_config)
@@ -59,9 +103,11 @@ config.command(name="set", help="Modify the configuration.")(conf.set_config)
 # gprof_nn training
 ######################################################################
 
+
 @gprof_nn.group(name="training")
 def train():
     pass
+
 
 train.command(name="init")(training.init_cli)
 train.command(name="run")(training.run_cli)
---
+++ gprof_nn/download.py
@@ -0,0 +1,89 @@
+"""
+gprof_nn.download
+=================
+
+Functionality to download GPROF-NN retrieval models and auxiliary data.
+"""
+
+import logging
+from pathlib import Path
+from typing import Optional
+
+from filelock import FileLock
+from huggingface_hub import hf_hub_download, snapshot_download
+
+from gprof_nn.config import CONFIG
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+def download_model(sensor: str) -> Path:
+    """
+    Download model to gprof_nn model path and return local path.
+
+    Args:
+        sensor: The name of the sensor for which to download the model.
+
+    Return:
+        The local path of the model.
+    """
+    model = f"gprof_nn_3d_{sensor.name.lower()}.pt"
+    model_path = CONFIG.data.model_path
+
+    if not (model_path / model).exists():
+        LOGGER.info("Downloading model file %s to model path %s", model, model_path)
+        lock = FileLock((model_path / model).with_suffix(".lock"))
+        with lock:
+            hf_hub_download("simonpf/gprof_nn", filename=model, local_dir=model_path)
+    return model_path / model
+
+
+TEST_FILES = {
+    "gmi": {
+        "l1c": "1C-R.GPM.GMI.XCAL2016-C.20241008-S051110-E064423.060255.V07B.HDF5",
+        "preprocessor": "1C-R.GPM.GMI.XCAL2016-C.20241008-S051110-E064423.060255.V07B.pp",
+    },
+    "amsr2": {
+        "l1c": "1C.GCOMW1.AMSR2.XCAL2016-V.20241008-S064659-E082551.065921.V07A.HDF5",
+        "preprocessor": "1C.GCOMW1.AMSR2.XCAL2016-V.20241008-S064659-E082551.065921.V07A.pp",
+    },
+    "atms": {
+        "l1c": "1C.NOAA20.ATMS.XCAL2019-V.20241008-S054520-E072649.035695.V07A.HDF5",
+        "preprocessor": "1C.NOAA20.ATMS.XCAL2019-V.20241008-S054520-E072649.035695.V07A.pp",
+    },
+}
+
+
+def download_test_file(sensor: str, kind: str, dest: Optional[Path] = None) -> Path:
+    """
+    Download test input file for a given sensor.
+
+    Args:
+        sensor: The name of the sensor for which to download the model.
+        kind: 'l1c' or 'preprocessor' to download the L1C or preprocessor input file.
+        dest: Directory to which to download the file.
+
+    Return:
+        The local path of the input file.
+    """
+    if dest is None:
+        dest = Path(".")
+
+    test_file = TEST_FILES.get(sensor, {}).get(kind, None)
+    if test_file is None:
+        raise ValueError(f"No known test file for sensor '{sensor}' and kind '{kind}'.")
+    subfolder = f"test_data/{sensor}"
+    lock_file = (dest / test_file).with_suffix(".lock")
+    lock = FileLock(lock_file)
+    with lock:
+        LOGGER.info("Downloading test  file %s to %s", test_file, dest)
+        hf_hub_download(
+            "simonpf/gprof_nn",
+            filename=test_file,
+            subfolder=subfolder,
+            local_dir=dest,
+        )
+    lock_file.unlink()
+
+    return dest / subfolder / test_file
