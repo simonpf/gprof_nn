cac293bd3b0fe0a5eb4a55879fb10508570e0568
Simon Pfreundschuh <s.pfreundschuh@gmail.com>
2025-04-07 10:44:38 -0600

    Add test file.

---
+++ tests/test_integration.py
@@ -0,0 +1,34 @@
+"""
+Integration tests for gprof_nn package.
+
+This test file runs the retrieval for all released retrieval models.
+"""
+from gprof_nn.download import download_test_file
+from gprof_nn.retrieval import run_retrieval
+
+
+def test_retrieval_gmi():
+    """
+    Tests the retrieval for GMI.
+    """
+    test_file = download_test_file("gmi", "l1c")
+    results = run_retrieval(test_file)
+    assert len(results) > 0
+
+
+def test_retrieval_atms():
+    """
+    Tests the retrieval for ATMS.
+    """
+    test_file = download_test_file("atms", "l1c")
+    results = run_retrieval(test_file)
+    assert len(results) > 0
+
+
+def test_retrieval_amsr2():
+    """
+    Tests the retrieval for ATMS.
+    """
+    test_file = download_test_file("atms", "l1c")
+    results = run_retrieval(test_file)
+    assert len(results) > 0
