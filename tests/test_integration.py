315b3e7d28e3fb988a43dc49e15b6aa267720b6a
Simon Pfreundschuh <s.pfreundschuh@gmail.com>
2025-12-30 10:31:46 -0700

    Add integration test for SSMI.

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -32,6 +32,7 @@ def test_retrieval_atms():
     results = run_retrieval(test_file)
     assert len(results) > 0
 
+
 def test_retrieval_amsr2():
     """
     Tests the retrieval for AMSR2.
@@ -67,6 +68,19 @@ def test_retrieval_ssmis():
     assert len(results) > 0
 
     test_file = download_test_file("ssmis", "preprocessor")
+    results = run_retrieval(test_file)
+    assert len(results) > 0
+
+
+def test_retrieval_ssmi():
+    """
+    Tests the retrieval for SSMI.
+    """
+    test_file = download_test_file("ssmi", "l1c")
+    results = run_retrieval(test_file)
+    assert len(results) > 0
+
+    test_file = download_test_file("ssmi", "preprocessor")
     results = run_retrieval(test_file)
     assert len(results) > 0
 
